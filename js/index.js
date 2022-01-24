d96b451620b992fa87e501a143188a45f89d9f2a
Fabion Dautaj <fabion.dautaj@maggioli.it>
2022-01-24 19:43:55 +0100

    fixed map

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
-// document.addEventListener("DOMContentLoaded", function(){
-//    // add padding top to show content behind navbar
-//    navbar_height = document.querySelector('.navbar.fixed-top').offsetHeight;
-//    document.body.style.paddingTop = navbar_height + 'px';
-// });
+$(function () {
+	$(".hotpoint-1, .hotpoint-2").on("click", function (e) {
+		e.preventDefault();
+
+		$(".box").hide();
+
+		var box = $(this).data("box");
+
+		var hotpointOffset = $(this).offset();
+
+		var hotpointWidth = $(this).width();
+
+		var boxHeight = $("." + box).height();
+		var boxTop = hotpointOffset.top - boxHeight - 10;
+		var boxLeft = hotpointOffset.left + hotpointWidth + 10;
+
+		$("." + box).css("top", boxTop);
+		$("." + box).css("left", boxLeft);
+
+		$("." + box).show();
+	});
+
+	$(".close-box").on("click", function (e) {
+		e.preventDefault();
+
+		$(this).parents(".box").hide();
+	});
+
+	$(".mappa").on("click", function (e) {
+		e.preventDefault();
+
+		$(".box").hide();
+	});
+});
